19e6e18b310bd7280c0e55537526818ad8f4737b
Marc Marino <marc.a.marino@gmail.com>
2019-03-13 11:21:46 -0400

    worked out two loops

--- src/index.js
+++ src/index.js
@@ -29,6 +29,19 @@ class Board extends React.Component {
     }
   }
 
+  createGrid = () => {
+    let grid = [];
+
+    for (let i = 0; i < 3; i++) {
+      let squares = [];
+      for (let j = 0; j < 3; j++) {
+        squares.push(<span>{this.renderSquare(i*3+j)}</span>)
+      }
+      grid.push(<div className="board-row">{squares}</div>)
+    }
+    return grid
+  }
+
   renderSquare(i) {
     const squareStatus = this.isWinningSquare(i, this.props.squares) ? "winningSquare" : "square"
     return (
@@ -43,21 +56,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createGrid()}
       </div>
     );
   }
